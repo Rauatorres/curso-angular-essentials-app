9c6fd38b8847dc3abbcd8793926b10b063bc7749
rauatorres <rauatorres@gmail.com>
2026-05-13 13:09:20 -0300

    conteúdo dinâmico e eventos

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
+const random = Math.floor(Math.random() * DUMMY_USERS.length);
+
 @Component({
   selector: 'app-user',
   imports: [],
@@ -8,5 +10,13 @@ import { DUMMY_USERS } from '../dummy-users';
   styleUrl: './user.scss',
 })
 export class User {
-  selectedUser = DUMMY_USERS[0];
+  selectedUser = DUMMY_USERS[random];
+
+  get imagePath() {
+    return 'users/' + this.selectedUser.avatar;
+  }
+
+  onSelectUser() {
+    console.log('clicked!');
+  }
 }
